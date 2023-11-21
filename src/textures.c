4dc3d78c0c622ea7af2d2048d7bfd59696650837
guillaume polve gosselin <gpolveg@macbook-pro-de-guillaume.home>
2023-11-21 13:12:27 +0100

    Fixes in wich texture is chose(n?)(je me comprends)

--- src/textures.c
+++ src/textures.c
@@ -6,7 +6,7 @@
 /*   By: gpolve-g <gpolve-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:09:22 by gpolve-g          #+#    #+#             */
-/*   Updated: 2023/11/19 18:39:19 by gpolve-g         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:09:01 by gpolve-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 t_image	*ft_select_text(t_pose *var, t_data *data)
 {
-	if (var->w_side == 1)
+	if (var->w_side == 0)
 	{
-		if (var->map_y < (int)var->y)
+		if (var->map_x > (int)var->x)
 			return (&data->south);
 		else
 			return (&data->north);
 	}
 	else
 	{
-		if (var->map_x > (int)var->x)
+		if (var->map_y > (int)var->y)
 			return (&data->west);
 		else
 			return (&data->east);
